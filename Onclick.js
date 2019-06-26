0e9565598d1c91cb01e47384551edf7694ddc35d
Freddy <contactfreddy11@gmail.com>
2019-06-26 13:57:05 -0700

    need to work on mobile

---
+++ Onclick.js
@@ -0,0 +1,105 @@
+
+function myFunction() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/DealManagement.png' />";
+}
+
+function inventory() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/AuctionTool.png' />";
+}
+
+function record() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Recording.png' />";
+}
+
+function lenders() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Finance.png' />";
+}
+
+function booking() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Booking.png' />";
+}
+
+function sales() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Tasks.png' />";
+}
+
+function pay() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Pay.png' />";
+}
+
+function dmv() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/DMV.png' />";
+}
+
+function web() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Website.png' />";
+}
+
+function quickbooks() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Quickbooks.png' />";
+}
+
+function credit() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/CreditServices.png' />";
+}
+
+function reporting() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Reporting.png' />";
+}
+
+function compliance() {
+  document.getElementById("demo").innerHTML="<img src='./resources/pictures/Compliance.png' />";
+}
+
+function help() {
+  var x = document.getElementById("request");
+  if (x.style.display === "none") {
+    x.style.display = "flex"; }
+  else if (x.style.display === "flex") {
+    x.style.display = "flex";
+  }
+  else { x.style.display = "none";}
+};
+
+function back() {
+  window.location.reload();
+}
+
+function profSum() {
+  document.getElementById("text").innerHTML=
+  "<h2>&emsp;Results-driven Credit Card Processing Sales Executive with over eight years&apos; experience in sales. "
+  + "Highly skilled in cold and warm prospecting and capitalizing on sales opportunities to achieve sales goals. "
+  + "Demonstrated leadership skills that enable team members to achieve individual sales goals, enhance customer "
+  + "service, and drive profits. Automotive knowlege of sales process and operations. Has technical understanding "
+  + "and is bilingual. Seeking to start a lasting career with a DealerCenter.</h2>"
+}
+
+function skills() {
+    document.getElementById("text").innerHTML=
+    "<span>Phone skills</span> - Cold calls, prospecting, customer retention.<br>" +
+    "<br><span>Merchant Service sales</span> - Understands sales process, rates and compliance.<br>" +
+    "<br><span>Interpersonal skills</span> - Listens to understand and leading the converation.<br>" +
+    "<br><span>Resourcfulness</span> - Able to adabt to changes in roles and responsibilities.<br>" +
+    "<br><span>Automotive Sales</span> - Understands a dealerships sales process and oprations.<br>" +
+    "<br><span>Technical skills</span> - Microsoft Office Suite, CRM software, HTML5, CSS, JavaScript.<br>" +
+    "<br><span>Bilingual</span> - Fluent verbally and written both in English and Spanish.<br>" +
+    "<br><span>Self-starter</span> - Able to take initiative and find solutions with out guidance.<br>" +
+    "<br><span>Teamwork</span> - Motivate be example and encouragemnt.<br>"
+}
+
+function experience() {
+  document.getElementById("text").innerHTML=
+  "<span>Account Executive,</span> 12/2017 to current USBSI acquired by Elavon/U.S. Bank. - City of Industry, CA<br>"
+  + "<br>&#9672;&emsp;Acquire new merchant business partners through telemarketing by prospecting, cold-calling, "
+  + "and building a relationship with clients. Perform business-to-business sales explaining the value of "
+  + "our services to merchants and business owners.<br>"
+  + "<br>&#9672;&emsp;Ascertain the needs for Sure3 Online Restaurants Solutions in a wide range of businesses. Explain the "
+  + "details of credit card processing to clients.<br>"
+  + "<br>&#9672;&emsp;Prepare complete and correct documentation for administrative processing and approval.<br>"
+  + "<br>&#9672;&emsp;Develop a sales process and implement best practices. Gather market data in order to improve product offerings."
+}
+
+function interests() {
+  document.getElementById("text").innerHTML=
+  "Hi"
+}
