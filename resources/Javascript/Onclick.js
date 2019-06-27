62c70b913e0c2a16b373a0bb26f230c11e7f1001
Freddy <contactfreddy11@gmail.com>
2019-06-27 01:10:31 -0700

    web resume

--- resources/Javascript/Onclick.js
+++ resources/Javascript/Onclick.js
@@ -70,21 +70,21 @@ function profSum() {
   "<h2>&emsp;Results-driven Credit Card Processing Sales Executive with over eight years&apos; experience in sales. "
   + "Highly skilled in cold and warm prospecting and capitalizing on sales opportunities to achieve sales goals. "
   + "Demonstrated leadership skills that enable team members to achieve individual sales goals, enhance customer "
-  + "service, and drive profits. Automotive knowlege of sales process and operations. Has technical understanding "
-  + "and is bilingual. Seeking to start a lasting career with a DealerCenter.</h2>"
+  + "service, and drive profits. Automotive knowledge of sales process and operations. Has a technical understanding "
+  + "and is bilingual. I am seeking to start a lasting career with a DealerCenter.</h2>"
 }
 
 function skills() {
     document.getElementById("text").innerHTML=
     "<span>Phone skills</span> - Cold calls, prospecting, customer retention.<br>" +
     "<br><span>Merchant Service sales</span> - Understands sales process, rates and compliance.<br>" +
-    "<br><span>Interpersonal skills</span> - Listens to understand and leading the converation.<br>" +
-    "<br><span>Resourcfulness</span> - Able to adabt to changes in roles and responsibilities.<br>" +
+    "<br><span>Interpersonal skills</span> - Listens to understand and leads the conversation.<br>" +
+    "<br><span>Resourcefulness</span> - Able to adapt to changes in roles and responsibilities.<br>" +
     "<br><span>Automotive Sales</span> - Understands a dealerships sales process and oprations.<br>" +
     "<br><span>Technical skills</span> - Microsoft Office Suite, CRM software, HTML5, CSS, JavaScript.<br>" +
     "<br><span>Bilingual</span> - Fluent verbally and written both in English and Spanish.<br>" +
-    "<br><span>Self-starter</span> - Able to take initiative and find solutions with out guidance.<br>" +
-    "<br><span>Teamwork</span> - Motivate be example and encouragemnt.<br>"
+    "<br><span>Self-starter</span> - Able to take the initiative and find solutions without guidance.<br>" +
+    "<br><span>Teamwork</span> - Motivate by example and encouragement.<br>"
 }
 
 function experience() {
@@ -96,10 +96,34 @@ function experience() {
   + "<br>&#9672;&emsp;Ascertain the needs for Sure3 Online Restaurants Solutions in a wide range of businesses. Explain the "
   + "details of credit card processing to clients.<br>"
   + "<br>&#9672;&emsp;Prepare complete and correct documentation for administrative processing and approval.<br>"
-  + "<br>&#9672;&emsp;Develop a sales process and implement best practices. Gather market data in order to improve product offerings."
+  + "<br>&#9672;&emsp;Develop a sales process and implement best practices. Gather market data to improve product offerings.<br>"
+
+  +"<br><br><span>Automotive Internet Sales Manger,</span> 01/2016 to 10/2017 Diamond Honda - City of Industry, CA<br>"
+  +"<br>&#9672;&emsp;Maintain currency on industry trends, new and pre-owned vehicle inventory, and individual vehicle features and options.<br>"
+  +"<br>&#9672;&emsp;Assist appointments choice a vehicle that best fits their needs while following a sale process to maximize both profits and customer satisfaction.<br>"
+  +"<br>&#9672;&emsp;Exceed monthly sales quota while mandating exceptional customer satisfaction survey’s scores.<br>"
+  +"<br>&#9672;&emsp;Prospect using proven phone scripts and emails to customers in service, as well as Internet inquiries (Edmund’s, Kelly Blue Book, AutoTrader, Honda, TrueCar websites).<br>"
+  +"<br>&#9672;&emsp;Help clients finance or lease by understanding their issues and getting the correct paperwork for the banks.<br>"
+  +"<br>&#9672;&emsp;Upsell service contracts and vehicle added accessories by demonstrating their value.<br>"
+
+  +"<br><br><span>Automotive Internet</span> Sales & Business Development Rep, 06/2013 to 12/2015 Nelson Honda - El Monte, CA<br>"
+  +"<br>&#9672;&emsp;Respond to warm Internet leads and customer questions, including inbound calls, regarding vehicle availability, price, and options; manage inquiries from Edmunds, Kelly Blue Book (KBB), AutoTrader, Honda, and TrueCar websites.<br>"
+  +"<br>&#9672;&emsp;Schedule prospective customer appointments to direct sales traffic to the dealership. Follow up with unsold traffic to understand their buying process and help reverse initial decision. "
+}
+
+function success() {
+  document.getElementById("text").innerHTML=
+    "<span>Identifying opportunities</span><br>"
+  +"<br>&emsp;The more people I talk to the more I sell. Even when I'm off work, I network what I do. I was out getting a smog for my car when I asked the owner who does his credit card processing and if he's happy. He tells me that his funds take too long and that he is paying too much. I set up another day I could talk to since I had to go to the DMV. When we did meet, I asked him about his fees and service. After understanding his problems, I brought up if I could help him would he do his processing with me. I then show him where I would be able to save him money, told him exactly how quickly his funds were going to come in and reassured him, he could always reach out to me when he is unhappy about something. He signed up with me because I was able to provide more value for his business. I recently went back to smog another car, and he is still happy.<br>"
+
+  +"<br><span>Persistence</span><br>"
+  +"<br>&emsp;I had been talking to an auto repair shop owner for some time. Every time I called, it was the same story. In Spanish, he kept telling me he was interested, but he needed to speak to his accountant. I even offered to talk to the accountant myself if he felt more comfortable. On a Friday, I decided to stop by and meet him. He was sincere and apologized that he hadn't had the chance to talk to his accountant that he had cars he was working on. He then wanted me to come back Monday, so we set up an appointment. I showed up early in the morning at the time we agreed, and no one was there. I decided to wait since it was early in the day. Finally, someone shows up. I introduce myself in Spanish and he happened to be the son of the owner. That his father died over the weekend. I apologized and told him I had met his father and why we were meeting. I asked him if it was ok to check on him in a week or so. He said that it was ok. I called back again in two weeks, just talked about the business and shared my condolences.  He then brings up he would need to wait to change everything in his name before he could do anything. I told him that's ok when he feels ready I could help. I follow up with once in a while for the next few months. Finally, he calls me back and tells me to meet him and the accountant. The auto repair shop signed up to do there credit card processing through me, and the accountant ended up also singing up his insurance company and later another business.<br>"
+
+  +"<br><span>Customer service</span><br>"
+  +"<br>&emsp;I had a towing company that does $60k a month in processing sign up there credit card processing with me. The owners then call me upset telling me 20k is missing. I didn't know what was going on, but I put myself in the shoes of the business. I let them know I understood that the situation was upsetting. That I will do everything I can to fix it quickly. Well, lost prevention wanted to talk to them and decided to hold $20k of there funds. At first, it was to verify that the charges weren't fraudulent. I was able to get all the documents we needed from the business except the phone number of the cardholder they were trying to verify. The business owners were so pissed that they were cursing at the person investigating their case over the phone. After that phone call, Elavon was going to close that account. They had put the funds in a reserve, and I didn't know what to do. The owners were telling me they were going to cancel. I was apologizing and telling them I don't blame them for being upset that I felt responsible and will try to escalate to the right people. After some back in forth between deparments, and channels, the issue was that ower told lost prevention that the company gets a lot of chargebacks. After some headache, I was able to prove to lost prevention through bank statements that the company doesn't get that many chargebacks. This whole issue took about three weeks to figure out. The owners did not like the lady at lost prevention but decided to stay with me because I understood their frustrations. I was able to save the business and was later given a referral because I can be trusted to be on top any issues that come up right away. "
 }
 
 function interests() {
   document.getElementById("text").innerHTML=
-  "Hi"
+  "<h2>&emsp;Outside of work, I enjoy camping and hiking. I have hiked to the bottom of the Grand Canyon and camped at the reservation by Havasu falls. Hobbies I would like to get back into is jiu-jitsu and photography. Beyond that, I enjoy reading books, mostly psychology/sociology. Some of my recommendations would be The Social Animal, A short history of nearly everything, Psycho-Cybernetics, Thinking Fast and Slow, Switch, and for fun, The Walking Dead Comics. My favorite movie would be The Departed while I still really enjoy superhero movies. I have traveled out outside the content and hope to get the chance to do more. While I love making time for a concert with friends, I find pride in challenging myself in learning programming languages. I am also close to my family, and we commonly get together for holidays or sporting events. My favorite is when we throw a big Dia De Los Muertos party.</h2>"
 }
